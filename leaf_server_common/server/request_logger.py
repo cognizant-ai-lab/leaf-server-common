b3e1b465ec4c8447aad39e37ccfa953546735d66
Sid Stuart <sidstuart@ip-172-31-32-15.us-west-2.compute.internal>
2025-10-30 22:29:38 +0000

    Fixed the little c in the copyright, updated pyprojec

--- leaf_server_common/server/request_logger.py
+++ leaf_server_common/server/request_logger.py
@@ -1,4 +1,5 @@
-# Copyright 2019-2025 Cognizant Technology Solutions Corp, www.cognizant.com.
+
+# Copyright © 2019-2025 Cognizant Technology Solutions Corp, www.cognizant.com.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#  END COPYRIGHT
+# END COPYRIGHT
 
 from typing import Dict
 
